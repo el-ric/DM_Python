5537a6df3de39edb2e3d5765520760e01359f0d2
Ricardo Avila <ricardo.avila@rye.mx>
2016-12-16 21:41:58 +0000

    Graph + Code changes

--- python_2811.py
+++ python_2811.py
@@ -1,88 +1,167 @@
 import pandas
 import random
 import math
-
-#filename = "GadgetManiacs_Cluster.xlsx"
-#filename = "CleanSupermarket.xlsx"
-#
-#dataDf = pandas.read_excel(filename)
-#data = dataDf.values.tolist()
-lines = len(data)
-columns = len(data[0])
-#print("\nNumber of lines:", lines)
-#print("Number of columns:", columns)
-
-clusterColumns = []
-clusterColumns=[2, 4, 6, 3]
-k = 3
+import matplotlib.pyplot as plot
 
 
-#SELECTS THE SAMPLE WITH ONLY THE COLUMNS TO CLUSTER
-selectedData = [[] for i in range(lines)]
-for i in range(lines):
-    selectedData[i] = [data[i][0]]
-    for j in range(len(clusterColumns)):
-        selectedData[i].append(data[i][clusterColumns[j]])
+def plotClusters(centroids, clusters,runNumber):
+    colors = ["b", "g", "r","y"]
+    colorsCluster = ["g", "r","b","y"]
+#    markers = ["o", "o", "o"]
+#    markers = ["^", "s", ""]
 
-        
-#SELECTS FROM THE SELECTED DATA K CENTROIDS
-initialCentroids = [[] for i in range(k)]
-for i in range(k):
-    r = random.randint(0, lines)
-    initialCentroids[i] = selectedData[r]
+    fig, ax = plot.subplots()
+    index = 0
+    ax.set_title('K-Means # {}'.format(runNumber))
+    for cluster in clusters:
+        for client in cluster:
+            ax.scatter(client[1], client[2], color=colors[clusters.index(cluster)], s=100, marker=",")
+            #ax.annotate(str(point[1]), (point[1] + 1, point[2] + 1))
+            index = (index + 1) % len(colors)
+    index = 0
+    for centroid in centroids:
+        ax.scatter(centroid[1], centroid[2], color=colorsCluster[index], s=500, marker="x")
+        ax.annotate("C" + str(index + 1), (centroid[1] + 2, centroid[2] + 2))
+        index = (index + 1) % len(colors)
 
-#######DO THIS UNTIL CONVERGENCE!#########
-#CALCULATE THE EUCLIDEAN DISTANCE FROM EACH POINT IN THE DATASET TO EACH CLUSTER POINT
-distance = [[] for i in range(lines)]
-for i in range(lines):       
-    distanceAux = 0.0
-    for j in range(k):
-        for l in range(1, len(clusterColumns)):
-            distanceAux += math.pow(selectedData[i][l] - initialCentroids[j][l], 2)
-        distanceAux = math.sqrt(distanceAux)
-        distance[i].append(distanceAux)
+    fig.canvas.draw()
+    fig.show()
 
-#CHECKS THE INDEX OF THE SMALLEST DISTANCE
-clusterAttribution = []
-for i in range(lines):
-    minDistance = distance[i].index(min(distance[i]))
-    clusterAttribution.append(minDistance)
+
+def readDataSet(filename, clusterColumns):
+    print("Reading file...")    
+    dataDf = pandas.read_csv(filename)
+    data = dataDf.values.tolist()
+    global lines 
+    lines = len(data)
+    columns = len(data[0])
+    initialCluster = 0
+    print("Number of lines:", lines)
+    print("Number of columns:", columns)   
+    #SELECTS THE SAMPLE WITH ONLY THE COLUMNS TO CLUSTER
+    selectedData = [[] for i in range(lines)]    
+    for i in range(lines):
+        selectedData[i] = [data[i][0]]
+        for j in range(len(clusterColumns)):
+            selectedData[i].append(data[i][clusterColumns[j]])
+        selectedData[i].append(initialCluster)
+    
+    return selectedData
+  
+def selectInitialCentoid(selectedData):
+    ##SELECTS RANDOM K CENTROIDS FROM THE NEW SET
+    initialCentroids = [[] for i in range(k)]
+    for i in range(k):
+        r = random.randint(0, lines)
+        initialCentroids[i] = selectedData[r]
+        if (offsetIdClient == 0):
+            initialCentroids[i].pop(0)
+    #
+    print ("Initial centroid selected")
+    print(initialCentroids)
+    return initialCentroids
+    
+def assignCluster(initialCentroids):
+    ########DO THIS UNTIL CONVERGENCE!#########
+    ##CALCULATE THE EUCLIDEAN DISTANCE FROM EACH POINT IN THE DATASET TO EACH CLUSTER POINT
+    distance = [[] for i in range(lines)]
+    #Every row / Client
+    for client in range(lines):       
+        distanceAux = 0.0
+    #    distance[i].append(i)
+        #Every cluster needed
+        for cluster in range(k):
+            #All the columns in the centroids
+            for column in range(1, len(clusterColumns)):
+                distanceAux += math.pow(selectedData[client][column] - initialCentroids[cluster][column], 2)
+            distanceAux = math.sqrt(distanceAux)
+            distance[client].append(distanceAux)
+    #Distance from one point to all the others
+    #print(distance)
+            
+    ##GETS THE CORRESPONDING CLUSTER FOR EACH CLIENT
+    #clusterAttribution = []
+    reassignedClients = 0
+    oldCluster = 0
+    for client in range(lines):
+        clusterNumber = distance[client].index(min(distance[client]))
+        oldCluster =  selectedData[client][len(selectedData[client])-1]
+        selectedData[client][len(selectedData[client])-1] = clusterNumber
+        if(oldCluster != clusterNumber):
+            reassignedClients += 1
+    print("reassignedClients")
+    print(reassignedClients)
+    #creates a list for each cluster with the client details --Easy to do math operations on separate lists.
+    #THIS PART MIGHT BE DUPICATED; WE ARE GETTING THE CLUSTER ID ON TWO VARIABLES
+    clusters = [[] for i in range(k)]  
+    for client in selectedData:
+        clusters[client[len(client) - 1]].append(client)
+
+    return clusters
 
     
-#ITERATES OVER THE LIST OF CLUSTERS AND UPDATES CENTROIDS
-updatedCentroids = [[0 for l in range (len(clusterColumns))] for i in range (k)]
+    
+def updateCentroid(updatedCentroids,clusters):
+    sumVal = [[0 for l in range (len(clusterColumns))] for i in range (k)]
+    countVal =  [[0 for l in range (len(clusterColumns))] for i in range (k)]
+    for cluster in clusters:
+        #print ("Working on cluster {}".format(clusters.index(cluster)))
+        for client in cluster:
+            # -2 because we have 2 extra columns
+            for availableColumns in range(len(client)- 2):
+                # +1 offsetIdClient because the first element of the client is the ID.
+                sumVal[clusters.index(cluster)][availableColumns] += client[availableColumns + offsetIdClient]
+                countVal[clusters.index(cluster)][availableColumns] += 1 
+    print("Initial clustering", updatedCentroids)
+    #print("sumVal",sumVal)
+    #print("countVal",countVal)
+    #plotClusters(updatedCentroids, selectedData)
+    for cluster in clusters:
+        for availableColumns in range(len(clusterColumns)):
+            if(countVal[clusters.index(cluster)][availableColumns]>0):
+                #+1 offsetIdClient because the first element of the client is the ID.
+                updatedCentroids[clusters.index(cluster)][availableColumns + offsetIdClient] = sumVal[clusters.index(cluster)][availableColumns] / countVal[clusters.index(cluster)][availableColumns]
 
-for i in range(lines):
-    for j in range(k): 
-        if clusterAttribution[i] == j:
-            for l in range (len(clusterColumns)):
-                updatedCentroids[j][l] += selectedData[i][l+1]
+    print("After",updatedCentroids)
+    return updatedCentroids
 
-print("Sum: ", updatedCentroids)
+    
+def showResults():
 
-#calculates the number of individuals in each cluster
-numberOfIndividuals = []
-for i in range(k):
-    temp = clusterAttribution.count(i)
-    numberOfIndividuals.append(temp)
-
-print(numberOfIndividuals)
-
-#calculates the average value for each position of each centroid
-for i in range(k):
-    for j in range(len(clusterColumns)):
-        updatedCentroids[i][j] = updatedCentroids[i][j]/numberOfIndividuals[i]
-
-print("Average: ", updatedCentroids)
-
-##we need to store the old centroid values so that we can compare the new centroids and
-##the old centroids to check for convergence - something like, if this is the first time 
-##the code runs, the old centroids is the initial random points; if this is not the first
-##time, the old centroids are the previous centroids.
+    plotClusters(centroids, clusters,numberIterations)
+    for i in range(k):
+        print("Cluster {} has {} elements".format(i+1,len(clusters[i])))  
+        
+    
+    #plotClusters(initialCentroids, selectedData)    
+            
+####GENERAL PARAMETERS ######
+filename = "DatasetClean.csv"
+clusterColumns = []
+#clusterColumns=[3,4,1]
+#We need to set this to 1 if we are reading the ID of the client.
+offsetIdClient = 1
+clusterColumns=[46, 81,83]
+k = 4
+numberIterations = 10
 
 
 
 
+print ("Running K-Means with K =  {} and {} variables ".format(k,len(clusterColumns)-1))
+selectedData = readDataSet(filename, clusterColumns)
+centroids = selectInitialCentoid(selectedData)
 
 
+
+for i in range(1, numberIterations):
+    print("Run ", i)
+    #Creates List with clusters and the clients in each cluster. 
+    #Updates the original selectedData with the current number of cluster for the client.
+    clusters = assignCluster(centroids)
+    centroids = updateCentroid(centroids,clusters)
+
+    
+showResults()
+    
 
